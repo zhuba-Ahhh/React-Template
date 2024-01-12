9783ed3abe9ed06d5940d4864cd7dcbd4be02db4
zhuba-Ahhh <84793349+zhuba-Ahhh@users.noreply.github.com>
2024-01-12 07:45:58 +0000

    Deploying to gh-pages from @ zhuba-Ahhh/React-Template@3e06284f8bb890b9172b9fbafa6dd5b6015816ac 🚀

---
+++ assets/views/Home/index.module-hS_zqkci.js
@@ -0,0 +1,2 @@
+const _={home:"_home_1xgbq_1",logo:"_logo_1xgbq_1",react:"_react_1xgbq_9",card:"_card_1xgbq_12","read-the-docs":"_read-the-docs_1xgbq_15","logo-spin":"_logo-spin_1xgbq_1"};export{_ as H};
+//# sourceMappingURL=index.module-hS_zqkci.js.map
